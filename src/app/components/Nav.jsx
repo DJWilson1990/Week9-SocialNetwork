d27ba0931ca38916edfc0d5f15ff10b073477d82
DANIEL WILSON <daniel.j.wilson@btinternet.com>
2024-03-06 09:28:32 +0000

    clerk, pages, nav

---
+++ src/app/components/Nav.jsx
@@ -0,0 +1,30 @@
+import { UserButton } from "@clerk/nextjs";
+import { SignInButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs";
+import Link from "next/link";
+
+export default function Nav() {
+  const { userId } = auth();
+  console.log("userId=" + userId);
+  return (
+    <div className="flex flex-row bg-slate-500">
+      <ul>
+        <li className="m-2 hover:text-white">
+          <Link href="/">HOME</Link>
+        </li>
+        <li className="m-2 hover:text-white">
+          <Link href="/profile">PROFILE</Link>
+        </li>
+        <li className="m-2 hover:text-white">
+          <Link href="/timeline">TIMELINE</Link>
+        </li>
+        <li className="m-2 hover:text-white">
+          <Link href="/friends">FRIENDS</Link>
+        </li>
+        <li className="m-2 hover:text-white">
+          {userId ? <UserButton /> : <SignInButton />}
+        </li>
+      </ul>
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <p className="color: bg-green-500">hello!</p>
+      <p className="color: bg-green-500">hello! This is the home page</p>
     </div>
   );
 }
