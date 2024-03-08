c0b0d837fd163f6fe30ee4bbcf4077624938fa37
DANIEL WILSON <daniel.j.wilson@btinternet.com>
2024-03-08 10:41:56 +0000

    img on post

--- src/app/components/DisplayPostHeader.jsx
+++ src/app/components/DisplayPostHeader.jsx
@@ -1,6 +1,14 @@
+import Image from "next/image";
+
 export default function DisplayPostHeader({ userName, imgURL }) {
   return (
     <div>
+      <Image
+        src={profile.image_link ?? "https://placehold.co/20x20/png"}
+        alt="profile picture"
+        width={20}
+        height={20}
+      />
       <h3>{userName}</h3>
     </div>
   );
