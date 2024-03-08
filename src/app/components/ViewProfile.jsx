b8b2188cdb62433fee541bbc5332e68ee4d98da1
DANIEL WILSON <daniel.j.wilson@btinternet.com>
2024-03-08 10:21:31 +0000

    testing

--- src/app/components/ViewProfile.jsx
+++ src/app/components/ViewProfile.jsx
@@ -2,21 +2,18 @@ import { getProfile } from "@/utils/utils";
 import Image from "next/image";
 import React from "react";
 
-export default async function ViewProfile() {
-  const profileData = await getProfile();
+export default async function ViewProfile({ profile }) {
+  console.log("profile component" + profile);
+  // const profileData = await getProfile();
   return (
     <div>
-      {profileData.map((profile) => (
-        <div key={profile.id}>
-          <Image src={profile.image_link} alt="profile picture" />
-          <p>{profile.first_name}</p>
-          <p>{profile.last_name}</p>
-          <p>{profile.email}</p>
-          <p>{profile.gender}</p>
-          <p>{profile.location}</p>
-          <p>{profile.occupation}</p>
-        </div>
-      ))}
+      {/* <Image src={profile.image_link} alt="profile picture" /> */}
+      <p>{profile.first_name}</p>
+      <p>{profile.last_name}</p>
+      <p>{profile.email}</p>
+      <p>{profile.gender}</p>
+      <p>{profile.location}</p>
+      <p>{profile.occupation}</p>
     </div>
   );
 }
--- src/app/profile/[userId]/page.js
+++ src/app/profile/[userId]/page.js
@@ -2,15 +2,17 @@
 
 import DisplayPost from "@/app/components/DisplayPost";
 import ViewProfile from "@/app/components/ViewProfile";
-import { getUserPosts } from "@/utils/utils";
+import { getProfile, getUserPosts } from "@/utils/utils";
 
-export default async function page({ userId }) {
-  const posts = await getUserPosts({ userId });
+export default async function page({ params }) {
+  const userProfile = await getProfile(params.userId);
+  console.log(userProfile);
+  const posts = await getUserPosts(params.userId);
 
   return (
     <div>
       <p>This will be a page to view a profile and associated posts!</p>
-      {/* <ViewProfile /> */}
+      <ViewProfile profile={userProfile} />
       {posts.map((post) => (
         <div key={post.id}>
           <DisplayPost post={post} />
